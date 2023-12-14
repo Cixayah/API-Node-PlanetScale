a7f632f81ca0592c7133e6c0a937c32c48ef6bf3
Cix <gabrielviniciodacosta@gmail.com>
2023-12-14 16:19:39 -0300

    #

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const server = express();
+
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: false }));
-
 server.use('/api', routes)
 
 server.listen(process.env.PORT, () => {
