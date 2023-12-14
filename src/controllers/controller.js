5b3ab2df9db73c1ebb0366345ad4dee613191af3
Cix <gabrielviniciodacosta@gmail.com>
2023-12-14 15:39:32 -0300

    #

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,1 +1,16 @@
 const service = require('../services/service');
+
+module.exports = {
+    getAll: async (req, res) => {
+        let json = { error: '', result: [] };
+        let carros = await service.getAll();
+
+        for (let i in carros) {
+            json.result.push({
+                codigo: carros[i].codigo,
+                descricao: carros[i].modelo
+            })
+        }
+        res.json(json);
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./controllers/controller');
+
+router.get('/carros', controller.getAll);
 module.exports = router;
